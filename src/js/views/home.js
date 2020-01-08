6773cc57f8ea58fbf551cf28ab9994d5b8733148
Giancarlo Tricerri <giankitricerri@gmail.com>
2020-01-08 23:36:13 +0000

    los mesoneros

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import petrico from "../../img/descarga.jpeg";
 import "../../styles/home.scss";
+import { Comofunciona } from "../component/Comofunciona";
 
 export const Home = () => (
 	<div>
@@ -12,7 +13,9 @@ export const Home = () => (
 		<div className="text-center mt-5">
 			<h1>Bievenido a... CasaCadena</h1>
 		</div>
-
+		<div>
+			<Comofunciona />
+		</div>
 		<div className="parallax" />
 	</div>
 );
