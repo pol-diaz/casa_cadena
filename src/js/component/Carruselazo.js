a031794f5f54bbc67f2dda44573524d78544ccfa
Giancarlo Tricerri <giankitricerri@gmail.com>
2020-01-09 22:33:04 +0000

    carruselazo

---
+++ src/js/component/Carruselazo.js
@@ -0,0 +1,40 @@
+import { Card } from "react-bootstrap";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+export const Carruselazo = props => {
+	return (
+		<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+			<div className="carousel-inner">
+				<div className="carousel-item active">
+					<img
+						className="d-block w-100"
+						src="https://i.pinimg.com/originals/49/81/1b/49811bd2de9f997fa0b0e561441624ed.jpg"
+						alt="First slide"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						className="d-block w-100"
+						src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/08/fondos-pantalla-full-hd-animales_4.jpg"
+						alt="Second slide"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						className="d-block w-100"
+						src="https://www.topmanzana.com/Imagenes/10-mejores-apps-fondos-hd-iphone-ipad.jpg"
+						alt="Third slide"
+					/>
+				</div>
+			</div>
+			<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+				<span className="carousel-control-prev-icon" aria-hidden="true" />
+				<span className="sr-only">Previous</span>
+			</a>
+			<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+				<span className="carousel-control-next-icon" aria-hidden="true" />
+				<span className="sr-only">Next</span>
+			</a>
+		</div>
+	);
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,14 +2,13 @@ import React from "react";
 import petrico from "../../img/descarga.jpeg";
 import "../../styles/home.scss";
 import { Comofunciona } from "../component/Comofunciona";
+import { Carruselazo } from "../component/Carruselazo";
 
 export const Home = () => (
 	<div>
 		<div className="parallax" />
 
-		<div className="logo">
-			<img src={petrico} />
-		</div>
+		<Carruselazo />
 		<div className="text-center mt-5">
 			<h1>Bievenido a... CasaCadena</h1>
 		</div>
