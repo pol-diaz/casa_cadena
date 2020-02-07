3c358e01626a1aaffa6d412ab1f7439598a97cc4
Paul <Pauldiaz693@gmail.com>
2020-02-07 19:24:35 -0400

    arreglos en el navbar

--- src/js/component/MyNavbar.js
+++ src/js/component/MyNavbar.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Form, FormControl, Button, Navbar } from "react-bootstrap";
-import logo from "../../img/logo-icon-purple.png";
+import logo from "../../img/logo-icon-white.png";
 
 export const MyNavbar = () => {
 	return (
-		<Navbar bg="light" expand="lg">
+		<Navbar className="cc-navbar" bg="light" expand="lg">
 			<Navbar.Brand href="/">
 				<img src={logo} style={{ width: 80 + "px", border: 0 + "px" }} />
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="mr-auto">
-					<Nav.Link href="#que-hacemos">{"¿Qué Hacemos?"}</Nav.Link>
-					<Nav.Link href="#como-funciona">{"¿Cómo Funciona?"}</Nav.Link>
-					<Nav.Link href="#tutoriales">{"Tutoriales"}</Nav.Link>
-					<Nav.Link href="#inicia-sesion">{"Inicia Sesión"}</Nav.Link>
-					<Nav.Link href="#registrate">{"Regístrate"}</Nav.Link>
+					<Nav.Link className="cc-navlink" href="#que-hacemos">
+						{"¿Qué Hacemos?"}
+					</Nav.Link>
+					<Nav.Link className="cc-navlink" href="#como-funciona">
+						{"¿Cómo Funciona?"}
+					</Nav.Link>
+					<Nav.Link className="cc-navlink" href="#tutoriales">
+						{"Tutoriales"}
+					</Nav.Link>
+					<Nav.Link className="cc-navlink" href="#inicia-sesion">
+						{"Inicia Sesión"}
+					</Nav.Link>
+					<Nav.Link className="cc-navlink" href="#registrate">
+						{"Regístrate"}
+					</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
