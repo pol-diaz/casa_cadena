d84e06d44a3d9dc1bb8cff8b259cd35f2c4b1b2f
Paul <Pauldiaz693@gmail.com>
2020-02-12 19:14:37 -0400

    updating

--- src/js/views/homeView.js
+++ src/js/views/homeView.js
@@ -10,14 +10,8 @@ export const HomeView = () => (
 	<div>
 		<MyNavbar />
 		<MyCarousel />
-<<<<<<< HEAD
 		<GetStarted />
-=======
-		<div className="text-center banner-styles">
-			<h1 className="banner-h1-styles">Bievenido a... CasaCadena</h1>
-		</div>
 		<MyCards />
->>>>>>> cc8e8a5a9b780db70cf47a40ea2ac023c87b7e77
 		<MyFooter />
 	</div>
 );
