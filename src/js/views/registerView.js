04230177e6bbe447f1c778ca0e58d56fd484fabf
Giancarlo Tricerri <giankitricerri@gmail.com>
2020-02-18 23:25:43 +0000

    vainas agregadas

--- src/js/views/registerView.js
+++ src/js/views/registerView.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "../../styles/register.scss";
 import { RegisterForm } from "../component/RegisterForm";
+import { MyFooter } from "../component/MyFooter";
 import { MyNavbar } from "../component/MyNavbar";
 
 export const RegisterView = () => {
 	return (
 		<div>
 			<MyNavbar />
+			<h2 className="sign-in-title-styles mx-auto my-2">Crea tu cuenta</h2>
 			<RegisterForm />
+			<MyFooter />
 		</div>
 	);
 };
