d76c61e284e1a923a5f50650d4e2be9e8fc71a9f
tommygonzaleza <gonzalez.tommy2001@gmail.com>
2020-02-28 03:51:12 +0000

    AdCard done

--- src/js/views/loggedInView.js
+++ src/js/views/loggedInView.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../../styles/logged-in.scss";
 import { MyNavbar } from "../component/MyNavbar";
+import { AdCard } from "../component/AdCard";
+import { MyFooter } from "../component/MyFooter";
 
 export const LoggedInView = () => {
 	return (
 		<React.Fragment>
-			<div />
+			<MyNavbar />
+			<AdCard />
 		</React.Fragment>
 	);
 };
